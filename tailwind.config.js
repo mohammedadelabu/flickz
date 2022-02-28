26361c50bb992c99c9e85012cdb7bb669eb94b6c
Emperor Hassy <mohammed.hassan@decagon.dev>
2022-02-28 09:45:08 +0100

    login and register feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
+// module.exports = {
+//   // purge: {
+//     content: [
+//       './src/**/*.{html,ts}'
+//     ],
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {
+//       opacity: ['disabled'],
+//       backgroundColor: ['disabled']
+//     },
+//   },
+//   plugins: [],
+// }
+
 module.exports = {
-  content: [
-    './src/**/*.{html,ts}'
-  ],
+  purge: {
+    content: [
+      './src/**/*.{html,ts}'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      backgroundColor: ['disabled']
+    },
   },
   plugins: [],
 }
